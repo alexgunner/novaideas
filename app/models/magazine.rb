11a0b105c63d434337d173bb4af8ad21c6d90c9f
Alexander Peredo Torossova <aptpta@yahoo.es>
2017-02-24 18:21:15 -0400

    Arreglo en  subir PDFs

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,4 +1,4 @@
 class Magazine < ApplicationRecord
   has_attached_file :mag
-  validates_attachment_content_type :mag, content_type: { content_type: "application/pdf" }
+  do_not_validate_attachment_file_type :mag
 end
